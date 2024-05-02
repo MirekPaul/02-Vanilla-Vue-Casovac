6b17b8f0c8448c6af9e9b611faaa467bc764b5a8
MirekPaul <mirekpaul@seznam.cz>
2024-05-02 10:46:47 +0200

    uspesna synchronizace

--- script.js
+++ script.js
@@ -17,11 +17,22 @@ setInterval(()=>{
 	vysl[1].innerText = (nahodneCislo % 2 == 0) ? "sudé" : "liché";
 	vysl[2].innerText = (nahodneCislo < 25) ? "je" : "není";
 	
+	//zmena smeru animace
+	//reseno tak, ze jsou ve stylu napsane animace pro oba smery zvlast - to ma vyhodu, ze pri prirazeni animace (nove) zacina vzdy od keyframe 0%, tim dojde k jejimu resetovani a zaroven synchronizovani s casovacem JS
+	if (nahodneCislo % 2 == 0) {		
+		sudaNeboLicha.style.animationName = "sudlich";
+	}
+	else {		
+		sudaNeboLicha.style.animationName = "lichsud"
+	}
 
 },2500)
 
 //selektovani vystupnich poli
 let vysl = document.querySelectorAll(".vysledek");
+
+//selektovani animacnich pumprdliku
+let sudaNeboLicha = document.querySelector(".sudeliche");
 
 //vystupni pole zprvnu naplnime rucne - kod je stejny jako ten v casovaci
 vysl[0].innerText = nahodneCislo;
